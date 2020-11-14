14b3fc32f2ee2636d9ca550a5216390aa9a2a6f5
vagnerRubens <55748747+vagnerRubens@users.noreply.github.com>
2020-11-14 18:04:16 -0300

    Trabalho pronto(mudar a fonte)

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {AppAuth, AppLoading} from 'expo'
 import {useFonts, Anton_400Regular} from "@expo-google-fonts/anton"
+// import {useFonts, nomedaminhafont} from "@expo-google-fonts/(Nomedafont)"
 
 import Routes from './src/router'
 
 export default function App() {
 
   let [fontsLoaded] = useFonts({
-    Anton_400Regular,
+    Anton_400Regular, 
+    // (nomedaminhafont)
   })
 
   if(!fontsLoaded){
--- src/vagner/component/cardPessoas.js
+++ src/vagner/component/cardPessoas.js
@@ -6,11 +6,21 @@ const EquipeListItem = (props) => {
     const { equipe, onPressItemDetails } = props
     const { id } = equipe.person.id
 
-    var imagem =  equipe.person.image.medium  
+    var imagem = ""
+
+    if(equipe.person.image === null && equipe.person.gender === 'Female'){
+        imagem = 'https://www.dinamize.com.br/wp-content/uploads/2016/10/man-3.png'
+    }else if(equipe.person.image === null && equipe.person.gender === 'Male'){
+        imagem ='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTenfEl-b_QiEs6hMLJDnYXRP8WzMWarpve_Q&usqp=CAU'
+    }else if(equipe.person.image === null && equipe.person.gender === null){
+        imagem = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRtHz0_U8J-W86rWKVoS2xqqGUcSMvc0DrIw&usqp=CAU"
+    }else{
+        imagem = equipe.person.image.medium
+    }  
     return (
         <TouchableOpacity style={styles.container} onPress={() => {onPressItemDetails(equipe)}}>
             <View style={styles.card} key={id}>
-                {imagem != null && (<Avatar.Image size={100} source={{uri: imagem.medium }} />)}               
+                {imagem != null && (<Avatar.Image size={100} source={{uri: imagem }} />)}               
                 <Title style={styles.text}>{equipe.person.name}</Title>
                 <Paragraph style={styles.text}>{equipe.person.type}</Paragraph>
             </View>
